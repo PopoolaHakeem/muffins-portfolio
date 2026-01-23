dc1ef6a5e9b8c48532813263e961305e3554852b
PopoolaHakeem <hakeempopoola04@gmail.com>
2026-01-23 16:58:10 +0100

    Nav Adjustment

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const closeBtn = document.querySelector('#closeBtn');
 const openBtn = document.querySelector('#menuBtn');
 
 closeBtn.addEventListener('click', () => {
-    // alert ('hello')
-    mobileMenu.classList.remove('hidden')
-    openBtn.classList.add('hidden')
+    alert ('hello')
+    // mobileMenu.classList.remove('hidden')
+    // openBtn.classList.add('hidden')
 
 })
