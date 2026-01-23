88fab2f3b1cca036ec316dbbbe8187e8db6f5a08
PopoolaHakeem <hakeempopoola04@gmail.com>
2026-01-23 17:15:23 +0100

    Nav Adjustment

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const mobileMenu = document.querySelector('.mobileMenu');
-const closeBtn = document.querySelector('#closeBtn');
-const openBtn = document.querySelector('#menuBtn');
+// const mobileMenu = document.querySelector('.mobileMenu');
+// const closeBtn = document.querySelector('#closeBtn');
+// const openBtn = document.querySelector('#menuBtn');
 
 
-openBtn.addEventListener('click', () => {
-    mobileMenu.classList.remove('hidden')
+// openBtn.addEventListener('click', () => {
+//     mobileMenu.classList.remove('hidden')
 
-})
+// })
 
-closeBtn.addEventListener('click', () => {
-    mobileMenu.classList.add('hidden')
-    openBtn.classList.remove('hidden')
-})
+// closeBtn.addEventListener('click', () => {
+//     mobileMenu.classList.add('hidden')
+//     openBtn.classList.remove('hidden')
+// })
