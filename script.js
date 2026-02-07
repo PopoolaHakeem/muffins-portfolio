122097f0cb87a164d0aaa28bac4408cd4851c69b
PopoolaHakeem <hakeempopoola04@gmail.com>
2026-02-07 14:37:31 +0100

    Education

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ closeBtn.addEventListener('click', () => {
     mobileMenu.classList.add('hidden')
     openBtn.classList.remove('hidden')
 })
+
+const navBg = document.querySelector('#navBg');
+window.addEventListener('scroll', () => {
+    if( window.scrollY > 1) {
+        navBg.style.backgroundColor = '#141414'
+        navBg.style.boxshadow = '0 2px 3px rgba(0 0 0 0.3)'
+        navBg.style.transition = '700 ease_in_out'
+    }
+    else {
+        navBg.style.backgroundColor = 'transparent'
+        // navBg.style.transitionTimingFunction = ' 300 ease_in_out';
+    }
+})
