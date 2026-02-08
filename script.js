1dd38f0c035ffc5d7cd5ce354a0f1c97e2595539
PopoolaHakeem <hakeempopoola04@gmail.com>
2026-02-08 20:16:57 +0100

    Testimonial

--- script.js
+++ script.js
@@ -26,3 +26,56 @@ window.addEventListener('scroll', () => {
         // navBg.style.transitionTimingFunction = ' 300 ease_in_out';
     }
 })
+
+
+
+
+  const track = document.getElementById('testimonialTrack');
+  const slides = track.children;
+
+  let index = 0;
+  let startX = 0;
+  let currentX = 0;
+  let isDragging = false;
+  let autoSlide;
+
+  function updateSlide() {
+    track.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  function next() {
+    index = (index + 1) % slides.length;
+    updateSlide();
+  }
+
+  // Touch (mobile swipe)
+  track.addEventListener('touchstart', e => {
+    startX = e.touches[0].clientX;
+    isDragging = true;
+    clearInterval(autoSlide);
+  });
+
+  track.addEventListener('touchmove', e => {
+    if (!isDragging) return;
+    currentX = e.touches[0].clientX;
+  });
+
+  track.addEventListener('touchend', () => {
+    if (!isDragging) return;
+
+    const diff = startX - currentX;
+
+    if (diff > 70) index = Math.min(index + 1, slides.length - 1);
+    if (diff < -70) index = Math.max(index - 1, 0);
+
+    updateSlide();
+    isDragging = false;
+    startAuto();
+  });
+
+  function startAuto() {
+    autoSlide = setInterval(next, 6000);
+  }
+
+  startAuto();
+
